d3a717c7f45ea9a3f1d2a0401cc5b8e95a78d53c
imat19 <vergara.reymart82@gmail.com>
2022-12-20 16:25:47 +0800

    added 6 slides

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,11 +1,17 @@
 function Rooms() {
   return (
     <>
-      <div className="container-lg mt-5">
+      <div className="container-fluid mt-5">
         <div className="row">
-          <h1 className="mb-0">Seaside Villas</h1>
-          <div className="col-fluid-10 d-flex justify-content-center">
-            <div id="carouselExampleIndicators" class="carousel slide mt-1" data-bs-ride="true">
+
+        {/*carousel 1 */}
+          <div className="col-md-4 mx-5">
+            <h1 className="m-5">Superior Room</h1>
+            <p>Air-conditioned, 42-inch Smart cabled TV, Wi-Fi, coffee and tea facility, mini-refrigerator, in-room safe, writing desk, bath and toilet separate, bidet, hair dryer, slippers. Hot and cold shower, bathroom amenities and towels.</p>
+            <button className="btn btn-primary"> BOOK NOW</button>
+          </div>
+          <div className="col-md-7">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
               <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -13,13 +19,13 @@ function Rooms() {
               </div>
               <div class="carousel-inner">
                 <div class="carousel-item active">
-                  <img src="https://www.lihim.com/wp-content/uploads/2022/10/Seaside-Villas-Outdoor-Bath.jpg" class="d-block  w-100" alt="..." />
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/2702272c.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
                 </div>
                 <div class="carousel-item">
-                  <img src="https://www.lihim.com/wp-content/uploads/2022/10/Seaside-Villas-Overview.jpg" class="d-block w-100" alt="..." />
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/f9b53db4.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
                 </div>
                 <div class="carousel-item">
-                  <img src="https://www.lihim.com/wp-content/uploads/2022/10/Seaside-Villas-Bedroom.jpg" class="d-block w-100 " alt="..." />
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/202cdcf8.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
                 </div>
               </div>
               <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -32,10 +38,196 @@ function Rooms() {
               </button>
             </div>
           </div>
+
+     {/*carousel 2 */}
+          <div className="col-md-7">
+            <div id="carouselExampleIndicators2" class="carousel slide" data-bs-ride="true">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/2702272c.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/f9b53db4.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/202cdcf8.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+          <div className="col-md-4 mx-5">
+            <h1 className="m-5">Superior Room</h1>
+            <p>Air-conditioned, 42-inch Smart cabled TV, Wi-Fi, coffee and tea facility, mini-refrigerator, in-room safe, writing desk, bath and toilet separate, bidet, hair dryer, slippers. Hot and cold shower, bathroom amenities and towels.</p>
+            <button className="btn btn-primary"> BOOK NOW</button>
+          </div>
+
+           {/*carousel 3 */}
+           <div className="col-md-4 mx-5">
+            <h1 className="m-5">Superior Room</h1>
+            <p>Air-conditioned, 42-inch Smart cabled TV, Wi-Fi, coffee and tea facility, mini-refrigerator, in-room safe, writing desk, bath and toilet separate, bidet, hair dryer, slippers. Hot and cold shower, bathroom amenities and towels.</p>
+            <button className="btn btn-primary"> BOOK NOW</button>
+          </div>
+          <div className="col-md-7">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/2702272c.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/f9b53db4.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/202cdcf8.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+
+         {/*carousel 4 */}
+         <div className="col-md-7">
+            <div id="carouselExampleIndicators2" class="carousel slide" data-bs-ride="true">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/2702272c.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/f9b53db4.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/202cdcf8.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+          <div className="col-md-4 mx-5">
+            <h1 className="m-5">Superior Room</h1>
+            <p>Air-conditioned, 42-inch Smart cabled TV, Wi-Fi, coffee and tea facility, mini-refrigerator, in-room safe, writing desk, bath and toilet separate, bidet, hair dryer, slippers. Hot and cold shower, bathroom amenities and towels.</p>
+            <button className="btn btn-primary"> BOOK NOW</button>
+          </div>
+
+          {/*carousel 5 */}
+          <div className="col-md-4 mx-5">
+            <h1 className="m-5">Superior Room</h1>
+            <p>Air-conditioned, 42-inch Smart cabled TV, Wi-Fi, coffee and tea facility, mini-refrigerator, in-room safe, writing desk, bath and toilet separate, bidet, hair dryer, slippers. Hot and cold shower, bathroom amenities and towels.</p>
+            <button className="btn btn-primary"> BOOK NOW</button>
+          </div>
+          <div className="col-md-7">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/2702272c.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/f9b53db4.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/202cdcf8.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+
+           {/*carousel 4 */}
+         <div className="col-md-7">
+            <div id="carouselExampleIndicators2" class="carousel slide" data-bs-ride="true">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/2702272c.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/f9b53db4.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://images.trvl-media.com/lodging/84000000/83730000/83720300/83720252/202cdcf8.jpg?impolicy=resizecrop&rw=1200&ra=fit" class="d-block w-100" alt="..." height={500} />
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators2" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+          <div className="col-md-4 mx-5">
+            <h1 className="m-5">Superior Room</h1>
+            <p>Air-conditioned, 42-inch Smart cabled TV, Wi-Fi, coffee and tea facility, mini-refrigerator, in-room safe, writing desk, bath and toilet separate, bidet, hair dryer, slippers. Hot and cold shower, bathroom amenities and towels.</p>
+            <button className="btn btn-primary"> BOOK NOW</button>
+          </div>
+          
         </div>
       </div>
+
+      
+
+
+
+
+
+
+
       <div>
         <div className="container-lg mt-5">
+          <h1 className="text-center">Amenities and Delight</h1>
           <div className="row">
             <div className="col-md-4">
               <h3 className="text-center"> 👩‍⚕️</h3>
@@ -82,7 +274,9 @@ function Rooms() {
               <h5 className="text-center">Butler Service</h5>
               <p className="mx-5">Indulge in tailored-to-fit services from Lihim Butlers who ensure all your requests are catered to with efficiency, excellent standards, and always with a personal</p>
             </div>
-           
+            <hr />
+            <p className="text-center">If you need further assistance and would like to know more details, kindly send an e-mail to: reservations@resortvillia.com</p>
+
           </div>
         </div>
       </div>
