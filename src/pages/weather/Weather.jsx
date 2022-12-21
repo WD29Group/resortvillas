3b1e1823363f8bae044fb6dad7a563dd051395a9
AloSamaa <kaizkiegt@gmail.com>
2022-12-21 08:34:07 +0800

    weather api data

---
+++ src/pages/weather/Weather.jsx
@@ -0,0 +1,13 @@
+import Carousel from "../../ReUse/Carousel.jsx";
+import "../css/weather.css";
+
+
+function Weather() {
+  return (
+    <>
+    <div className="bkg1"></div>
+    <Carousel />
+    </>
+  );
+}
+export default Weather;
