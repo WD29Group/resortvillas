a8320fa778a681c801f93fe0020d177d03f3ac85
AloSamaa <kaizkiegt@gmail.com>
2022-12-27 12:49:24 +0800

    stopped using useeffect in weather

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,22 +1,20 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import Carousel from "../ReUse/Carousel.jsx";
 
 function Weather() {
     const [data, setData] = useState(null);
-    const icon = useRef(null);
+    const [icon, setIcon] = useState(null);
 
-    useEffect(() => {
-        async function fetchData() {
-            const response = await fetch(
-                'https://api.weatherbit.io/v2.0/current?city=El-Nido&key=0f2c78fe24d04bc8969b4c9cbb0b72c5&include=minutely',
-
-            );
-            const json = await response.json();
-            setData(json);
-            icon.current = require(`../img/icons/${data.data[0].weather.icon}.png`);
-        }
-        fetchData()
-    }, []);
+    async function fetchData() {
+        const response = await fetch(
+            'https://api.weatherbit.io/v2.0/current?city=El-Nido&key=0f2c78fe24d04bc8969b4c9cbb0b72c5&include=minutely',
+            
+        );
+        const json = await response.json();
+        setData(json);
+        setIcon(require(`../img/icons/${data.data[0].weather.icon}.png`));
+    }
+    fetchData();
 
     const temp = data ? data.data[0].app_temp : '35';
     const description = data ? data.data[0].weather.description : 'Cloudy';
@@ -30,7 +28,7 @@ function Weather() {
                     <div className="row">
                         <div className="weatherbod col-md-4">
                             <div className="col-md-12">
-                                <span className="fw-bold h1 pt-5 ps-5 pe-5">{temp} °C </span> <img className="ms-5 ps-5"src={icon.current} alt="..." />
+                                <span className="fw-bold h1 pt-5 ps-5 pe-5">{temp} °C </span> <img className="ms-5 ps-5"src={icon} alt="..." />
                                 <br />
                                 <span className="ps-5">{description} at {city}, {country}</span>
                             </div>
@@ -42,13 +40,12 @@ function Weather() {
                                 <span className="ps-5">{description} at {city}, {country}</span>
                             </div>
                             <div className="col-md-3">
-                                <img src={icon.current} alt="..." />
+                                <img src={icon} alt="..." />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </>
     );
 }
