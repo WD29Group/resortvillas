14d06f6f5d53f9c9339ecf35703702290eb38efe
AloSamaa <kaizkiegt@gmail.com>
2022-12-27 12:33:25 +0800

    new layout for weather

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -9,14 +9,14 @@ function Weather() {
         async function fetchData() {
             const response = await fetch(
                 'https://api.weatherbit.io/v2.0/current?city=El-Nido&key=0f2c78fe24d04bc8969b4c9cbb0b72c5&include=minutely',
-                
+
             );
             const json = await response.json();
             setData(json);
             icon.current = require(`../img/icons/${data.data[0].weather.icon}.png`);
         }
         fetchData()
-    },[]);
+    }, []);
 
     const temp = data ? data.data[0].app_temp : '35';
     const description = data ? data.data[0].weather.description : 'Cloudy';
@@ -28,7 +28,14 @@ function Weather() {
             <div className="wBody">
                 <div className="container" id="weather1">
                     <div className="row">
-                        <div className="weatherbod">
+                        <div className="weatherbod col-md-4">
+                            <div className="col-md-12">
+                                <span className="fw-bold h1 pt-5 ps-5 pe-5">{temp} °C </span> <img className="ms-5 ps-5"src={icon.current} alt="..." />
+                                <br />
+                                <span className="ps-5">{description} at {city}, {country}</span>
+                            </div>
+                        </div>
+                        <div className="weatherbod col-md-8">
                             <div className="col-md-3">
                                 <span className="fw-bold h1 text-center pt-5 ps-5">{temp} °C </span>
                                 <br />
@@ -41,6 +48,7 @@ function Weather() {
                     </div>
                 </div>
             </div>
+
         </>
     );
 }
