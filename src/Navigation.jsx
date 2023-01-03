5dfca2be0dbde65535d9b3dc0554a589d8d0bbbf
wd29group <wd29group@gmail.com>
2023-01-04 00:00:06 +0800

    test19

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import { useState } from 'react';
 import ScrollToTopButton from './ReUse/ScrollToTopButton';
 import SpecialOffer from './ReUse/SpecialOffer';
-// import ChatSupport from './ReUse/ChatSupport';
 function Navigation({ children }) {
   const [selected, setSelected] = useState('*');
   console.log(selected);
@@ -198,7 +197,6 @@ function Navigation({ children }) {
           </Link>
         </li>
       </ul>
-      {/* <ChatSupport /> */}
       {children}
       <ScrollToTopButton />
     </>
