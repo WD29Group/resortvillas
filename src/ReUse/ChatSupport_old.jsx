ae6a3eb8be0ba9600640dafb644d6f4cd35f131e
wd29group <wd29group@gmail.com>
2023-01-03 23:47:19 +0800

    test17

--- src/ReUse/ChatSupport_old.jsx
+++ src/ReUse/ChatSupport_old.jsx
@@ -21,3 +21,19 @@ function ChatSupport() {
 export default ChatSupport;
 //mark.wd296449264861753855
 //m.me/112254835081759
+
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
+// import React from 'react';
+
+// function ChatSupport() {
+//   return (
+//     <MessengerCustomerChat
+//       pageId="112254835081759"
+//       appId="6449264861753855"
+//     />
+//   );
+// }
+
+// export default ChatSupport;
+// //mark.wd296449264861753855
+// //m.me/112254835081759
