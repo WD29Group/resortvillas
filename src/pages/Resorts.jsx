4e31170969f9d8cb6a60d24252707771aecaaf18
imat19 <vergara.reymart82@gmail.com>
2023-01-05 00:16:13 +0800

    added 10 hotels for resorts

--- src/pages/Resorts.jsx
+++ src/pages/Resorts.jsx
@@ -1,16 +1,43 @@
-// import MapLoc from "./MapLoc";
+import MapLoc from "./MapLoc";
 // import Rooms from "./Rooms";
-import DeluxeRoom from "./RoomPages/DeluxeRoom";
-function Resorts() {
-  return (
-    <>
-      <div>
+import { useState, useEffect } from "react";
+import hotelsVillas from "./hotelsVillas.json"
 
-       <DeluxeRoom />
-    
-      </div>
-     
-    </>
+function Resorts() {
+  useEffect(() => {
+    console.log(hotelsVillas)
+  });
+  return (
+    <div className="m-5">
+      <MapLoc />
+      {hotelsVillas.hotels.map((hotel) => {
+        return (
+          <>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-6">
+                  <div class="card">
+                    <img src={hotel.thumbnailUrl} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                      <h4 class="card-title">{hotel.title}</h4>
+                      <h5 class="card-title">{hotel.location}</h5>
+                      <p class="card-text">{hotel.content}</p>
+                      <p class="card-text">{hotel.facilities}</p>
+                      <p class="card-text">P {hotel.price} </p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </>
+
+        );
+        })};
+    </div>
+
+
   );
 }
 
--- src/pages/RoomPages/DeluxeRoom.jsx
+++ src/pages/RoomPages/DeluxeRoom.jsx
@@ -62,9 +62,9 @@ function DeluxeRoom() {
               <div className="row">
                 <div class="card col-md-4">
                   <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 class="card-title">👨</h5>
+                    <h5 class="card-subtitle mb-2"> Butler Service</h5>
+                    <p class="card-text">Indulge in tailored-to-fit services from Lihim Butlers who ensure all your requests are catered to with efficiency, excellent standards, and always with a personal touch.</p>
                   </div>
                 </div>
                 <div class="card col-md-4">
