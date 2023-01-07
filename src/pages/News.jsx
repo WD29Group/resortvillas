6ee54726c4a4df4f34ada56784a045062a4fd4f8
imat19 <vergara.reymart82@gmail.com>
2023-01-08 03:05:37 +0800

    added news page

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,6 +1,66 @@
 function News() {
   return (
-    <div className="bkg2"></div>
+    <div className="main-news">
+      <div className="bg-white">
+        <div className="container-xxl">
+          <div class="row">
+            <div class="col-md-9">
+              <div className="effect-news bg-white">
+                <img className="img-fluid " src="https://data-starcinema.abs-cbn.com/starcinema/starcinema/media/april-2022/25/arjo-atayde,-maine-mendoza-spend-quality-time-in-el-nido_1.jpg?ext=.jpg" alt="" />
+                <div className="mt-2 p-2">
+                  <h1>Arjo Atayde, Maine Mendoza spend quality time in El Nido</h1>
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem eos hic nemo facilis at temporibus quos necessitatibus quidem error tempora doloremque, consequatur placeat minus consequuntur exercitationem eligendi quisquam expedita asperiores.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="row">
+                <div className="me-2">
+                  <div class="effect-news col-md-12 bg-white p-0">
+                    <img className="three-cards img-fluid" src="https://mega.onemega.com/wp-content/uploads/2019/04/Nadine-Lustre-Palawan-1024x1024.jpg" alt="" />
+                    <p className="mt-1">Arjo Atayde, Maine Mendoza spend quality time in El Nido</p>
+                  </div>
+                  <div class="effect-news col-md-12  my-2 bg-white p-0">
+                    <img className="three-cards img-fluid" src="https://data-starcinema.abs-cbn.com/starcinema/starcinema/media/april-2022/30/bea-alonzo.jpg?ext=.jpg" alt="" />
+                    <p className="mt-1">Arjo Atayde, Maine Mendoza spend quality time in El Nido</p>
+                  </div>
+                  <div class="effect-news col-md-12 bg-white p-0">
+                    <img className="three-cards img-fluid" src="https://aphrodite.gmanetwork.com/entertainment/photos/photo/in_photos__celebrities_at_the_beautiful_beaches_arny_ross_1493193457.jpg" alt="" />
+                    <p className="mt-1">Arjo Atayde, Maine Mendoza spend quality time in El Nido</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="container-xxl">
+        <div class="row p-2">
+          <h3>Recent News</h3>
+          <div class="effect-news col-md-4 bg-white p-0">
+          <div className="mx-1">
+              <img className="three-cards img-fluid" src="https://pix10.agoda.net/hotelImages/9714/-1/5af56521ca524350683fa265ce95b9be.jpg?ca=7&ce=1&s=1024x768" alt="" />
+              <p className="mt-1">Arjo Atayde, Maine Mendoza spend quality time in El Nido</p>
+            </div>
+          </div>
+          <div class="effect-news col-md-4 bg-white p-0">
+          <div className="mx-1">
+              <img className="three-cards img-fluid" src="https://elnidoresortscareers.com/admin/uploads/about/155381974581846300.jpg" alt="" />
+              <p className="mt-1">Arjo Atayde, Maine Mendoza spend quality time in El Nido</p>
+            </div>
+          </div>
+          <div class="effect-news col-md-4 bg-white p-0">
+            <div className="mx-1">
+              <img className="three-cards img-fluid" src="https://media-cdn.tripadvisor.com/media/photo-s/16/03/e0/3d/miniloc-island-water.jpg" alt="" />
+              <p className="mt-1">Arjo Atayde, Maine Mendoza spend quality time in El Nido</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   );
 }
 
