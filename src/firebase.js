7f8e47c5877c5fbc0fbd83f506c79397937d7c4c
wd29group <wd29group@gmail.com>
2023-01-07 09:24:05 +0800

    update firebase acc

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCq0bA8dn8JXPm7S8VUw-Irh2u3w1HDv9w",
-  authDomain: "chat-app-ae0df.firebaseapp.com",
-  projectId: "chat-app-ae0df",
-  databaseURL: "https://chat-app-ae0df-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  storageBucket: "chat-app-ae0df.appspot.com",
-  messagingSenderId: "365867048984",
-  appId: "1:365867048984:web:6ec98bd9f8c7d01e56636d"
+  apiKey: "AIzaSyAUoU2ktY4aOsyOQghdVL9MmP_E6job0ag",
+  authDomain: "testchat-75af5.firebaseapp.com",
+  databaseURL: "https://testchat-75af5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "testchat-75af5",
+  storageBucket: "testchat-75af5.appspot.com",
+  messagingSenderId: "736041791961",
+  appId: "1:736041791961:web:f26342351713bafc25e9c8"
 };
 
 // Initialize Firebase
