3ecbe05b5816734bc55a7152812e02394bdc6b16
AloSamaa <kaizkiegt@gmail.com>
2023-01-09 09:09:43 +0800

    updated gallery

---
+++ src/img/gallery/index.js
@@ -0,0 +1,59 @@
+import img1 from "./gallery1.jpg";
+import img2 from "./gallery2.jpg";
+import img3 from "./gallery3.jpg";
+import img4 from "./gallery4.jpg";
+import img5 from "./gallery5.jpg";
+import img6 from "./gallery6.jpg";
+import img7 from "./gallery7.jpg";
+import img8 from "./gallery8.jpg";
+import img9 from "./gallery9.jpg";
+import img10 from "./gallery10.jpg";
+import img11 from "./gallery11.jpg";
+import img12 from "./gallery12.jpg";
+import img13 from "./gallery13.jpg";
+import img14 from "./gallery14.jpg";
+import img15 from "./gallery15.jpg";
+import img16 from "./gallery16.jpg";
+import img17 from "./gallery17.jpg";
+import img18 from "./gallery18.jpg";
+import img19 from "./gallery19.jpg";
+import img20 from "./gallery20.jpg";
+import img21 from "./gallery21.jpg";
+import img22 from "./gallery22.jpg";
+import img23 from "./gallery23.jpg";
+import img24 from "./gallery24.jpg";
+import img25 from "./gallery25.jpg";
+import img26 from "./gallery26.jpg";
+import img27 from "./gallery27.jpg";
+import img28 from "./gallery28.jpg";
+import img29 from "./gallery29.jpg";
+import img30 from "./gallery30.jpg";
+import img31 from "./gallery31.jpg";
+import img32 from "./gallery32.jpg";
+import img33 from "./gallery33.jpg";
+import img34 from "./gallery34.jpg";
+import img35 from "./gallery35.jpg";
+import img36 from "./gallery36.jpg";
+import img37 from "./gallery37.jpg";
+import img38 from "./gallery38.jpg";
+import img39 from "./gallery39.jpg";
+import img40 from "./gallery40.jpg";
+import img41 from "./gallery41.jpg";
+import img42 from "./gallery42.jpg";
+import img43 from "./gallery43.jpg";
+import img44 from "./gallery44.jpg";
+import img45 from "./gallery45.jpg";
+import img46 from "./gallery46.jpg";
+import img47 from "./gallery47.jpg";
+import img48 from "./gallery48.jpg";
+import img49 from "./gallery49.jpg";
+import img50 from "./gallery50.jpg";
+
+
+
+
+export { img1, img2, img3, img4, img5, img6, img7, img8, img9, img10,
+     img11, img12, img13, img14, img15, img16, img17, img18, img19, img20,
+      img21, img22, img23, img24, img25, img26, img27, img28, img29, img30,
+      img31, img32, img33, img34, img35, img36, img37, img38, img39, img40,
+      img41, img42, img43, img44, img45, img46, img47, img48, img49, img50};
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,68 +1,145 @@
+import React, { useState } from "react";
 import Footer from "../ReUse/Footer";
-import ChangeTopBG from "../ReUse/ChangeTopBG";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-// import * as imgs from "../img/gallery";
+import * as imgs from "../img/gallery";
 
 function Gallery() {
+  const [data, setData] = useState({img: '',i: 0})
+  const [style] = useState({});
+
+  const handleMouseEnter = (e) => {
+    e.target.style.transform = "scale(1.03)";
+    e.target.style.boxShadow = "0 0 10px rgba(0,0,0,0.5)";
+  };
+  
+  const handleMouseLeave = (e) => {
+    e.target.style.transform = "scale(1)";
+    e.target.style.boxShadow = "";
+  };
+  const viewImage = (img, i) =>{
+    setData({img, i})
+  }
+  
+  const imgAction = (action) =>{
+    let i = data.i;
+    if(action === 'next-img'){
+      setData({img: images[i+1], i:i + 1})
+    }
+    if(action === 'previous-img'){
+      setData({img: images[i-1], i:i - 1})
+    }
+    if(!action){
+      setData({img:'', i:0})
+    }
+  }
+
   const images = [
-    "https://picsum.photos/2000/2500",
-    //...
-    "https://picsum.photos/3000/3000",
-    "https://picsum.photos/2000/3500",
-    "https://picsum.photos/2500/3000",
-    "https://picsum.photos/2000/3000",
-    "https://picsum.photos/2000/3500",
-    "https://picsum.photos/2000/3500",
-    "https://picsum.photos/2500/3000",
-    "https://picsum.photos/3000/3000",
-    "https://picsum.photos/2000/3500",
-    "https://picsum.photos/2500/3000",
-    "https://picsum.photos/2000/3000",
-    "https://picsum.photos/2000/3500",
-    "https://picsum.photos/2000/3500",
-    "https://picsum.photos/2500/3000",
-    "https://picsum.photos/2500/3000",
+    imgs.img1,
+    imgs.img2,
+    imgs.img3,
+    imgs.img4,
+    imgs.img5,
+    imgs.img6,
+    imgs.img7,
+    imgs.img8,
+    imgs.img9,
+    imgs.img10,
+    imgs.img11,
+    imgs.img12,
+    imgs.img13,
+    imgs.img14,
+    imgs.img15,
+    imgs.img16,
+    imgs.img17,
+    imgs.img18,
+    imgs.img19,
+    imgs.img20,
+    imgs.img21,
+    imgs.img22,
+    imgs.img23,
+    imgs.img24,
+    imgs.img25,
+    imgs.img26,
+    imgs.img27,
+    imgs.img28,
+    imgs.img29,
+    imgs.img30,
+    imgs.img31,
+    imgs.img32,
+    imgs.img33,
+    imgs.img34,
+    imgs.img35,
+    imgs.img36,
+    imgs.img37,
+    imgs.img38,
+    imgs.img39,
+    imgs.img40,
+    imgs.img41,
+    imgs.img42,
+    imgs.img43,
+    imgs.img44,
+    imgs.img45,
+    imgs.img46,
+    imgs.img47,
+    imgs.img48,
+    imgs.img49,
+    imgs.img50,
+
+
+    // "https://picsum.photos/3000/3000",
+    // "https://picsum.photos/2000/3500",
+    // "https://picsum.photos/2500/3000",
+    // "https://picsum.photos/2000/3000",
+    // "https://picsum.photos/2000/3500",
+    // "https://picsum.photos/2000/3500",
+    // "https://picsum.photos/2500/3000",
+    // "https://picsum.photos/2500/3000",
   ];
 
   return (
-    <ResponsiveMasonry>
+    <>
+    {data.img &&
+    <div  style={{
+      width: '100%',
+      height: '100vh',
+      background: 'black',
+      position: 'fixed',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      overflow: 'hidden',
+      zIndex: 100,
+    }}>
+       <button onClick={() => imgAction('')}style={{position: 'absolute',top: '10px',right: '10px'}} className="p-1">x</button>
+       <button onClick={() => imgAction('previous-img')} className="me-3">Previous</button>
+       <img src={data.img} style={{width: 'auto', maxWidth: '90%', maxHeight: '90%'}} alt=""/>
+       <button onClick={() => imgAction('next-img')} className="ms-3">Next</button>
+    </div>
+    
+    }
+
+
       <Footer scrollAmount="0" setBGcolor="rgba(0, 0, 0, 0.50)" />
       <div id="Gbod">
         <div className="container Galbod">
-          <div className="text-center text-white h1 pb-5">El Nido, Palawan</div>
-          <Masonry columnsCount={3} gutter="10px">
+          <div className="text-center text-black h1 pb-5">El Nido, Palawan</div>
+          <ResponsiveMasonry>
+          <Masonry columnsCount={3} gutter="20px">
             {images.map((image, i) => (
               <img
-                key={i}
-                src={image}
-                style={{ width: "100%", display: "block" }}
-                alt="..."
-              />
+              src={image}
+              style={{ width: "100%", display: "block", cursor: "pointer", ...style }}
+              alt=""
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              onClick={() => viewImage(image, i)}
+            />
             ))}
-          </Masonry>
+            </Masonry>
+            </ResponsiveMasonry>
         </div>
       </div>
-      <ChangeTopBG scrollAmount="0" transitionDuration="0.5s" />
-    </ResponsiveMasonry>
+    </>
   );
 }
-
 export default Gallery;
-
-
-// import Elnidovid2 from "../ReUse/Elnidovid2";
-// import ContentsGalleryCards from "../contents/ContentsGalleryCards";
-// import ChangeTopBG from "../ReUse/ChangeTopBG";
-// import Footer from "../ReUse/Footer";
-
-// function Gallery() {
-//   return (
-//     <>
-//       <ChangeTopBG scrollAmount="0" transitionDuration="0.5s" />
-//       <ContentsGalleryCards />
-//       <Elnidovid2 />
-//       <Footer scrollAmount="0" setBGcolor="rgba(0, 0, 0, 0.50)" />
-//     </>
-//   );
-// }
-// export default Gallery;
