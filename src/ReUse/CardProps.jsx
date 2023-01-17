ea4cb2b450bdd85bacafe3f091317a68d1477721
wd29group <wd29group@gmail.com>
2023-01-17 13:00:19 +0800

    update for News allignments

--- src/ReUse/CardProps.jsx
+++ src/ReUse/CardProps.jsx
@@ -5,11 +5,11 @@ function CardProps(props) {
     
         <div className={props.column}>
           <div className="m-2 p-1 shadow-lg" >
-            <img className="img-fluid" src={props.scr} alt="" />
+            <img className="img-fluid" src={props.scr} alt="resortvillas" />
             <div className="p-2">
               <h5 className="mt-1 m-0 ">{props.title}</h5>
               <p className="col-md-12 m-0">{props.date}</p>
-              <p className="col-md-12">{props.body} <a className=" text-primary " href={props.href} target="_blank">see more,</a></p> 
+              <p className="col-md-12">{props.body} <a className=" text-primary " href={props.href}  rel="noopener">see more,</a></p> 
             </div>
           </div>
         </div>
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -10,21 +10,22 @@ function News() {
   return (
     <>
       <Subscribe />
+      <div id="breaking-news"></div>
       <div className=" contaier-fluid">
-        <img className="img-fluid" src="https://t4.ftcdn.net/jpg/02/65/26/83/360_F_265268314_LmykO3vrtzmh3TQbBdnxj9vUczqqJXCU.jpg" alt="" />
+        <img className="img-fluid" src="https://t4.ftcdn.net/jpg/02/65/26/83/360_F_265268314_LmykO3vrtzmh3TQbBdnxj9vUczqqJXCU.jpg" alt="resort villas" />
       </div>
       <div className="main-news mt-5">
-        <div className="forbackground pt-5" id="breaking-news">
+        <div className="forbackground pt-5">
           <div className="container-xxl">
             <div class="row">
-              {breakingNews.newsUpdates.filter(newsUpdates => newsUpdates.ids == 4).map((newsUpdate) => (
+              {breakingNews.newsUpdates.filter(newsUpdates => newsUpdates.ids === 4).map((newsUpdate) => (
                 <div class="col-md-8 p-1">
                   <div className="effect-news">
-                    <img className="main-pict img-fluid" src={newsUpdate.thumbnailUrl} alt="" />
+                    <img className="main-pict img-fluid" src={newsUpdate.thumbnailUrl} alt="resort villas" />
                     <div className="mt-2 p-2">
                       <h2>{newsUpdate.title}</h2>
                       <p>{newsUpdate.date}</p>
-                      <h6 className="p-2">>{newsUpdate.content}<a className="text-decoration-underline text-primary" href={"https://www.travelbubble.elnidoresorts.com/"} target="_blank">see more, </a></h6>
+                      <h6 className="p-2">>{newsUpdate.content}<a className="text-decoration-underline text-primary" href={"https://www.travelbubble.elnidoresorts.com/"}  rel="noopener">see more, </a></h6>
                     </div>
                   </div>
                 </div>
@@ -34,12 +35,12 @@ function News() {
               <div class="col-md-4">
                 <div class="row">
                   <div className="mb-2 p-0">
-                    {breakingNews.newsUpdates.filter(newsUpdates => newsUpdates.ids == 1).map((newsUpdate) => (
+                    {breakingNews.newsUpdates.filter(newsUpdates => newsUpdates.ids === 1).map((newsUpdate) => (
                       <div className="sideNews pb-1 mb-3">
-                        <img className="img-fluid three-cards m-0" src={newsUpdate.thumbnailUrl} />                       
+                        <img className="img-fluid three-cards m-0" src={newsUpdate.thumbnailUrl}  alt="resort villas" />                       
                         <h5 className="m-2">{newsUpdate.title}</h5>
                         <p className="m-2">{newsUpdate.date}</p>
-                        <p className="mb-2"><a className="text-decoration-underline text-primary" href={newsUpdate.link} target="_blank"> see more,</a></p>
+                        <p className="mb-2"><a className="text-decoration-underline text-primary" href={newsUpdate.link}  rel="noopener"> see more,</a></p>
                       </div>
                     ))}
                   </div>
@@ -48,13 +49,13 @@ function News() {
             </div>
           </div>
         </div>
-        <div className="container-fluid" id="breaking-news">
+        <div className="container-fluid">
           <div className="container-xxl py-3">
           
             {/* Breaking news */}
             <div class="row">
               <h3>Breaking News</h3>
-              {breakingNews.newsUpdates.filter(newsUpdates => newsUpdates.ids == 2).map((newsUpdate) => (
+              {breakingNews.newsUpdates.filter(newsUpdates => newsUpdates.ids === 2).map((newsUpdate) => (
                 <CardProps
                   column="effect-news col-md-6 p-0"
                   title={newsUpdate.title}
@@ -67,12 +68,12 @@ function News() {
             </div>
           </div>
         </div>
-        <div className="container-fluid" id="breaking-news">
+        <div className="container-fluid">
             {/* Recent news */}
         <div className="container-xxl">
           <div class="row p-2">
             <h3>Recent News</h3>
-            {breakingNews.newsUpdates.filter(newsUpdates => newsUpdates.ids == 3).map((newsUpdate) => (
+            {breakingNews.newsUpdates.filter(newsUpdates => newsUpdates.ids === 3).map((newsUpdate) => (
               <CardProps
                 column="effect-news  col-lg-4  p-2 pb-2"
                 title={newsUpdate.title}
