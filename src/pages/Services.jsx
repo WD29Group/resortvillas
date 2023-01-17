57372698b5734dcfa575182c1d40b5782a3fffd3
imat19 <vergara.reymart82@gmail.com>
2023-01-17 23:39:16 +0800

    testimonials

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,68 +1,9 @@
-import breakingNews from "./NewsAndUpdates/breakingNews.json";
 
 
 function Services() {
   return (
     <>
-     <div className="container-xxl">
-        <div className="row">
-          {breakingNews.newsUpdates.filter(newsUpdates=>newsUpdates.ids==1).map((newsUpdate) => (
-            <div className="col-12 col-md-6 mb-5">
-              <div className="recent-news">
-                <div className="card">
-                  <img src={newsUpdate.thumbnailUrl} class="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">{newsUpdate.title}</h5>
-                    <p className="card-text">{newsUpdate.content}</p>
-                    <p className="card-text">{newsUpdate.date}</p>
-                    <p>
-                      Read more{" "}
-                      <a
-                        className="card-text text-decoration-underline text-primary"
-                        href={newsUpdate.link}
-                        target="_blank"
-                      >
-                        see more
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="container-xxl">
-        <div className="row">
-          {breakingNews.newsUpdates.map((newsUpdate) => (
-            <div className="col-6 col-md-3 mb-5">
-              <div className="recent-news">
-                <div className="card">
-                  <img src={newsUpdate.thumbnailUrl} class="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">{newsUpdate.title}</h5>
-                    <p className="card-text">{newsUpdate.content}</p>
-                    <p className="card-text">{newsUpdate.date}</p>
-                    <p>
-                      Read more{" "}
-                      <a
-                        className="card-text text-decoration-underline text-primary"
-                        href={newsUpdate.link}
-                        target="_blank"
-                      >
-                        see more
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-       <div className="container-xxl">
-        
-      </div>
-      </div>
+    
     </>
   );
 }
--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,9 +1,107 @@
+
 function Weather() {
+
+
   return (
-    <><div className="container"></div>
-      <img src=" https://gttp.imgix.net/322040/x/0/41-best-palawan-resorts-el-nido-coron-puerto-princesa-port-barton-san-vicente-balabac-taytay-9?auto=compress%2Cformat&ch=Width%2CDPR&dpr=1&ixlib=php-3.3.0&w=883" alt="" />
-      <img src="https://gttp.imgix.net/322035/x/0/41-best-palawan-resorts-el-nido-coron-puerto-princesa-port-barton-san-vicente-balabac-taytay-4.jpg?auto=compress%2Cformat&ch=Width%2CDPR&dpr=1&ixlib=php-3.3.0&w=883" />
-      <img src="https://gttp.imgix.net/322072/x/0/41-best-palawan-resorts-el-nido-coron-puerto-princesa-port-barton-san-vicente-balabac-taytay-34.jpg?auto=compress%2Cformat&ch=Width%2CDPR&dpr=1&ixlib=php-3.3.0&w=883&fbclid=IwAR2jlwamLoArLq59j6rMGNXBhCa7MxeVOkbY7VKT74tNV-VCvitr9_ejaz0" />
+    <>
+      <div id="carouselExampleIndicators" class="carousel slide col-md-6" data-bs-ride="true">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner bg-white">
+          <div class="carousel-item active">
+
+            <div>
+            <div class="card  rounded-5 m-0 p-0 mb-5">
+                <div className="bg-warning text-center  rounded-5 py-1 "  >
+                  <img src="https://static.vecteezy.com/system/resources/previews/001/200/028/original/dog-png.png" class="testimonial-pict card-img-top border border-5  rounded-pill bg-warning p-2 " alt="..." />
+                  <h2 className="card-title mt-0 text-white"><b>Im Doggy awaw</b></h2>
+                  <h5 className="card-title text-white text-muted">Dog App Developer</h5>
+                </div>
+                <div class="body-size card-body  mt-0 p-0 px-2">
+
+                  <h5 class="card-text text-white text-muted m-3">
+                    <span className="large mb-2 ms-5">"</span> <br /> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus sapiente saepe officiis? Consequuntur ipsum dolorum consequatur voluptate corrupti quidem nulla? Maiores quasi possimus fugiat deserunt necessitatibus tempora quas fugit iste? <span className="large d-flex justify-content-end mx-5">"</span>
+
+                  </h5>
+                  <div className="text-center d-flex justify-content-center mb-5 text-muted">
+                    <i class="fa fa-facebook-square fa-2x"></i>
+                    <i class="fa fa-google-plus-square fa-2x mx-2"></i>
+                    <i class="fa fa-youtube-square fa-2x"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div class="carousel-size carousel-item">
+
+          <div>
+          <div class="card  rounded-5 m-0 p-0 mb-5">
+                <div className="bg-warning text-center  rounded-5 py-1 "  >
+                  <img src="https://static.vecteezy.com/system/resources/previews/001/200/028/original/dog-png.png" class="testimonial-pict card-img-top border border-5  rounded-pill bg-warning p-2 " alt="..." />
+                  <h2 className="card-title mt-0 text-white"><b>Im Doggy awaw</b></h2>
+                  <h5 className="card-title text-white text-muted">Dog App Developer</h5>
+                </div>
+                <div class="body-size card-body  mt-0 p-0 px-2">
+
+                  <h5 class="card-text text-white text-muted m-3">
+                    <span className="large mb-2 ms-5">"</span> <br /> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus sapiente saepe officiis? Consequuntur ipsum dolorum consequatur voluptate corrupti quidem nulla? Maiores quasi possimus fugiat deserunt necessitatibus tempora quas fugit iste? <span className="large d-flex justify-content-end mx-5">"</span>
+
+                  </h5>
+                  <div className="text-center d-flex justify-content-center mb-5 text-muted">
+                    <i class="fa fa-facebook-square fa-2x"></i>
+                    <i class="fa fa-google-plus-square fa-2x mx-2"></i>
+                    <i class="fa fa-youtube-square fa-2x"></i>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+
+          <div class="carousel-item">
+
+          <div>
+              <div class="card  rounded-5 m-0 p-0 mb-5">
+                <div className="bg-warning text-center  rounded-5 py-1 "  >
+                  <img src="https://static.vecteezy.com/system/resources/previews/001/200/028/original/dog-png.png" class="testimonial-pict card-img-top border border-5  rounded-pill bg-warning p-2 " alt="..." />
+                  <h2 className="card-title mt-0 text-white"><b>Im Doggy awaw</b></h2>
+                  <h5 className="card-title text-white text-muted">Dog App Developer</h5>
+                </div>
+                <div class="body-size card-body  mt-0 p-0 px-2">
+
+                  <h5 class="card-text text-white text-muted m-3">
+                    <span className="large mb-2 ms-5">"</span> <br /> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus sapiente saepe officiis? Consequuntur ipsum dolorum consequatur voluptate corrupti quidem nulla? Maiores quasi possimus fugiat deserunt necessitatibus tempora quas fugit iste? <span className="large d-flex justify-content-end mx-5">"</span>
+
+                  </h5>
+                  <div className="text-center d-flex justify-content-center mb-5 text-muted">
+                    <i class="fa fa-facebook-square fa-2x"></i>
+                    <i class="fa fa-google-plus-square fa-2x mx-2"></i>
+                    <i class="fa fa-youtube-square fa-2x"></i>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+
     </>
   );
 }
